53e6ccddb958fbec6d273407b5b676d2e8b8042c
Vinicius Ferreira Bezerra <vinicius.ferreira.bezerra07@aluno.ifce.edu.br>
2023-12-05 19:41:13 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const divImagemPrincipal = document.querySelector("#imagem-principal")
+const imagemPrincipal = divImagemPrincipal.querySelector("img")
+const textoAlternativo = divImagemPrincipal.querySelector(".texto-alternativo")
+const btnProximo = divImagemPrincipal.querySelector(".proximo")
+const btnAnterior = divImagemPrincipal.querySelector(".anterior")
+const todasImagens = document.querySelectorAll("imagens img")
+
+let idImagemAtiva = 0
+
+const proximaImagem = function () {
+    idImagemAtiva++
+    if (idImagemAtiva>= todasImagens.length) idImagemAtiva = 0
+    selecionarImagem()
+}
+
+const voltarImagem = function () {
+    idImagemAtiva--
+    if (idImagemAtiva < 0) idImagemAtiva = todasImagens.length - 1
+    selecionarImagem()
+}
+
+const selecionarImagem = function () {
+    imagemPrincipal.src = todasImagens[idImagemAtiva].src
+    todasImagens.forEach(function(imagem){
+        imagem.classList = "";
+    })
+    todasImagens[idImagemAtiva].classList.add("")
+}
